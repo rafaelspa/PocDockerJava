b50c7c72bfa209caf3d0aca97965d4b5dd2b419f
rafaelspa <rsdepaula67@gmail.com>
2023-06-15 18:33:06 -0300

    volumes listing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.command.CreateVolumeResponse;
+import com.github.dockerjava.api.command.InspectVolumeResponse;
 import com.github.dockerjava.api.command.PullImageResultCallback;
 import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.api.model.Frame;
@@ -13,10 +15,14 @@ import com.github.dockerjava.core.DockerClientImpl;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 // https://github.com/docker-java/docker-java
@@ -62,6 +68,27 @@ public class Main {
             System.out.println("Status: " + container.getStatus());
             System.out.println();
         }
+
+        System.out.println("\nVolume list");
+        List<InspectVolumeResponse> volumes = dockerClient.listVolumesCmd().withFilter("name", List.of("vol-whalesay")).exec().getVolumes();
+        if (Objects.nonNull(volumes) && volumes.size() > 0) {
+            for (var volume : volumes) {
+                System.out.println("Name: " + volume.getName());
+                System.out.println("Mountpoint: " + volume.getMountpoint());
+                System.out.println("Driver: " + volume.getDriver());
+                System.out.println("Mountpoint: " + volume.getMountpoint());
+                System.out.println("Labels: " + volume.getLabels());
+                System.out.println("Options: " + volume.getOptions());
+            }
+        } else {
+            CreateVolumeResponse volumeResponse = dockerClient.createVolumeCmd().withName("vol-whalesay").exec();
+            System.out.println("Name: " + volumeResponse.getName());
+            System.out.println("Mountpoint: " + volumeResponse.getMountpoint());
+            System.out.println("Driver: " + volumeResponse.getDriver());
+            System.out.println("Mountpoint: " + volumeResponse.getMountpoint());
+            System.out.println("Labels: " + volumeResponse.getLabels());
+        }
+
         try {
             boolean whalesayExists = images.stream().anyMatch(image -> Arrays.asList(image.getRepoTags()).contains("docker/whalesay:latest"));
             if (!whalesayExists) {
